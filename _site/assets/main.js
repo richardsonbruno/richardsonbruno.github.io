d9806880d24cf26d4a10846b037c5c22e9187564
Richardson Bruno <richardsonbruno@MacBook-Pro-de-Richardson.local>
2018-11-04 21:10:16 -0300

    Postagens e reparos nas páginas

--- _site/assets/main.js
+++ _site/assets/main.js
@@ -4,26 +4,6 @@
         document.querySelector("nav.header-nav").classList.toggle("active");
     });
 
-    document.querySelectorAll(".header-nav-link").forEach(function(index){
-
-        index.addEventListener("click", function(event) {
-            event.preventDefault();
-            let hash = this.hash.substr(1);
-            let block = document.querySelector(`.${hash}`);
-
-            document.querySelectorAll(".header-nav-link").forEach(function(index){
-                index.classList.remove("active");
-            });
-
-            this.classList.add("active");
-            
-            document.querySelectorAll(".block").forEach(function(index){
-                index.classList.remove("bounceOut", "bounceIn");
-                index.classList.add("bounceOut");
-                block.classList.remove("bounceOut");
-                block.classList.add("bounceIn");
-            });
-
-        });
-    });
+    let link = window.location.href.split("/")[3];
+    
 })();
